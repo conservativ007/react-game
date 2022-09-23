e97776359abbc54ef6804403b937a09ef10a71e9
conservativ <alisa3d2007@gmail.com>
2022-09-23 15:35:56 +0300

    changed settings screen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
-import footer from '../Styles/footer.css';
+import React, { useEffect, useRef, useState } from 'react';
+import '../styles/footer.css';
 
-export default function Footer({resetGame}) {
+export default function Footer({ resetGame }) {
+
+  let [isShowSettings, setIsShowSettings] = useState(false);
+  let buttonOfSetting = useRef(null);
+
+  function showSettings() {
+    document.querySelector(".settings")
+      .classList.toggle("show-settings");
+    setIsShowSettings(prev => !prev);
+  }
+
+  useEffect(() => {
+    isShowSettings === false ?
+      buttonOfSetting.current.className = "animate-down" :
+      buttonOfSetting.current.className = "animate-up";
+
+  }, [isShowSettings]);
+
   return (
     <div className='footer'>
       <div className="author">
@@ -9,12 +26,15 @@ export default function Footer({resetGame}) {
           <a href="https://github.com/conservativ007">Conservativ</a>
         </span>
       </div>
-      <div className="reset-game" onClick={() => resetGame()}>
-        начать занаво
+      <div className="reset-game">
+        <p onClick={() => resetGame()}>начать занаво</p>
+        <div
+          ref={buttonOfSetting}
+          onClick={() => showSettings()} >&#187;</div>
       </div>
       <div className="link">
         <a target='blink' href="https://rs.school/js/">
-          <img className='footer-img' src="https://rs.school/images/rs_school_js.svg" alt="rs_school"/>
+          <img className='footer-img' src="https://rs.school/images/rs_school_js.svg" alt="rs_school" />
         </a>
         <span className='year'>2021</span>
       </div>
--- src/components/function.js
+++ src/components/function.js
@@ -1,19 +1,19 @@
 function saveRecord(element) {
-  
-  let res = JSON.parse(localStorage.getItem ("records"));
-  if(element === 'abra-cadabra') return res;
 
-  if(res === null){
+  let res = JSON.parse(localStorage.getItem("records"));
+  if (element === 'abra-cadabra') return res;
+
+  if (res === null) {
     let records = {
       x: element === 'x' ? 1 : 0,
       o: element === 'o' ? 1 : 0,
     }
-    localStorage.setItem ("records", JSON.stringify(records));
+    localStorage.setItem("records", JSON.stringify(records));
   } else {
     let a = res[element];
     a++;
     res[element] = a;
-    localStorage.setItem ("records", JSON.stringify(res));
+    localStorage.setItem("records", JSON.stringify(res));
   }
   return res;
 }
@@ -33,60 +33,9 @@ function getSittingsInLocalStorage(value) {
   return res;
 }
 
-
-
 export {
-  saveRecord, 
-  getArrayInLocalStorage, 
-  getA1InLocalStorage, 
-  getSittingsInLocalStorage, 
+  saveRecord,
+  getArrayInLocalStorage,
+  getA1InLocalStorage,
+  getSittingsInLocalStorage,
 }
-// let [autoPlay, setAutoPlay] = useState(false);
-// useEffect(() => {
-
-//   if(autoPlay !== true) return;
-
-//   // let timerId = setInterval(() => func(), 2000)
-
-//   func();
-//   function func() {
-//     let randomNumber;
-//     let copy;
-
-//     function getRandomArbitrary(min, max) {
-//       return Math.random() * (max - min) + min;
-//     }
-
-//     function getRandomElementinCopy() {
-//       randomNumber = Math.trunc(getRandomArbitrary(0, 9));
-//       copy = arr.slice();
-
-//       return copy[randomNumber];
-//     }
-
-//     addArrElement()
-//     function addArrElement() {
-//       let elem = getRandomElementinCopy();
-//       const checkElemInNull = (elem) => elem === null;
-
-//       if(checkElemInNull(elem)) {
-//         copy[randomNumber] = a1;
-//         setArr(copy)
-//         setA1(a1 === 'x' ? 'o' : 'x');
-//         isWin(a1, copy);
-//         // console.log(a1)
-//       } else {
-//         addArrElement();
-//       }
-//     }
-
-//   }
-  
-//     // addArrElement();
-  
-
-//   // return () => clearInterval(timerId);
-  
-//   console.log(arr)
-// }, [autoPlay])
-
